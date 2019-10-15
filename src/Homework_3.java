695dac2e25cac215f5b9373e617d2d6185fff999
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-10-16 00:29:36 +0400

    Not optimized

---
+++ src/Homework_3.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+public class Homework_3 {
+    public static void main(String[] args) {
+        Scanner  scanner = new Scanner (System.in);
+        String change = new String ();
+        String [][] scedule = new String [7][2];
+        generate_scedule(scedule);
+        while (true)
+        {
+            System.out.println("\nPlease, input the day of the week:");
+            change = scanner.nextLine();
+
+            if (change.equals(change.toUpperCase()) ||
+                change.equals(change.toLowerCase()) ||
+                    change.equals(change + " ") ||
+                    change.equals(change.substring(0,1).toUpperCase()+change.substring(1)))
+            {
+                change = change.toLowerCase();
+                switch (change)
+                {
+                    case "monday":
+                        System.out.printf("Your tasks for %s is : %s",scedule [1][0],scedule [1][1]);
+                        break;
+
+                    case "tuesday":
+                        System.out.printf("Your tasks for %s is : %s",scedule [2][0],scedule [2][1]);
+                        break;
+
+                    case "wednesday":
+                        System.out.printf("Your tasks for %s is : %s",scedule [3][0],scedule [3][1]);
+                        break;
+
+                    case "thursday":
+                        System.out.printf("Your tasks for %s is : %s",scedule [4][0],scedule [4][1]);
+                        break;
+
+                    case "friday":
+                        System.out.printf("Your tasks for %s is : %s",scedule [5][0],scedule [5][1]);
+                        break;
+
+                    case "saturday":
+                        System.out.printf("Your tasks for %s is : %s",scedule [6][0],scedule [6][1]);
+                        break;
+
+                    case "sunday":
+                        System.out.printf("Your tasks for %s is : %s",scedule [0][0],scedule [0][1]);
+                        break;
+
+                    case "exit":
+                    System.exit(0);
+                    default:
+                        System.out.println("Sorry, I don't understand you, please try again");
+                } } }}
+
+    private static void generate_scedule(String[][] scedule) {
+        scedule[0][0] = "Sunday";
+        scedule[0][1] = "do home work";
+        scedule[1][0] = "Monday";
+        scedule[1][1] = "go to courses; watch a film";
+        scedule[2][0] = "Tuesday";
+        scedule[2][1] = "go to university; have a rest";
+        scedule[3][0] = "Wednesday";
+        scedule[3][1] = "go to courses;listen music";
+        scedule[4][0] = "Thursday";
+        scedule[4][1] = "go to university; have a rest";
+        scedule[5][0] = "Friday";
+        scedule[5][1] = "go to courses;read a book";
+        scedule[6][0] = "Saturday";
+        scedule[6][1] = "have a rest; watch TV;play Playstation";
+    }}
