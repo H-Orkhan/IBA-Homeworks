fb21eeddc642e36c66129bfc6bd6cf3704b903e9
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-10-16 13:08:06 +0400

    Fixed Bugs and optimized code

--- src/Homework_3.java
+++ src/Homework_3.java
@@ -5,52 +5,37 @@ public class Homework_3 {
         String change = new String ();
         String [][] scedule = new String [7][2];
         generate_scedule(scedule);
-        while (true)
-        {
-            System.out.println("\nPlease, input the day of the week:");
-            change = scanner.nextLine();
-
-            if (change.equals(change.toUpperCase()) ||
-                change.equals(change.toLowerCase()) ||
-                    change.equals(change + " ") ||
-                    change.equals(change.substring(0,1).toUpperCase()+change.substring(1)))
-            {
-                change = change.toLowerCase();
-                switch (change)
-                {
+        while (true) {
+            change = generate_change();
+                switch (change) {
                     case "monday":
-                        System.out.printf("Your tasks for %s is : %s",scedule [1][0],scedule [1][1]);
-                        break;
-
+                        System.out.printf("Your tasks for %s is : %s",scedule [1][0],scedule [1][1]);break;
                     case "tuesday":
-                        System.out.printf("Your tasks for %s is : %s",scedule [2][0],scedule [2][1]);
-                        break;
-
+                        System.out.printf("Your tasks for %s is : %s",scedule [2][0],scedule [2][1]);break;
                     case "wednesday":
-                        System.out.printf("Your tasks for %s is : %s",scedule [3][0],scedule [3][1]);
-                        break;
-
+                        System.out.printf("Your tasks for %s is : %s",scedule [3][0],scedule [3][1]);break;
                     case "thursday":
-                        System.out.printf("Your tasks for %s is : %s",scedule [4][0],scedule [4][1]);
-                        break;
-
+                        System.out.printf("Your tasks for %s is : %s",scedule [4][0],scedule [4][1]);break;
                     case "friday":
-                        System.out.printf("Your tasks for %s is : %s",scedule [5][0],scedule [5][1]);
-                        break;
-
+                        System.out.printf("Your tasks for %s is : %s",scedule [5][0],scedule [5][1]);break;
                     case "saturday":
-                        System.out.printf("Your tasks for %s is : %s",scedule [6][0],scedule [6][1]);
-                        break;
-
+                        System.out.printf("Your tasks for %s is : %s",scedule [6][0],scedule [6][1]);break;
                     case "sunday":
-                        System.out.printf("Your tasks for %s is : %s",scedule [0][0],scedule [0][1]);
-                        break;
-
+                        System.out.printf("Your tasks for %s is : %s",scedule [0][0],scedule [0][1]);break;
                     case "exit":
-                    System.exit(0);
+                    System.exit(0);break;
                     default:
                         System.out.println("Sorry, I don't understand you, please try again");
-                } } }}
+                } } }
+
+    private static String generate_change() {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("\nPlease, input the day of the week:");
+        String change = scanner.nextLine();
+        change = change.toLowerCase();
+        change = change.replace(" ","");
+        return change;
+    }
 
     private static void generate_scedule(String[][] scedule) {
         scedule[0][0] = "Sunday";
