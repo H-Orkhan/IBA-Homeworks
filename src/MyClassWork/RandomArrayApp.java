5de3e512a106b268d4013c821a2daee3b78574ec
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-10-21 16:21:52 +0400

    My homework4 (Do not optimized)

---
+++ src/MyClassWork/RandomArrayApp.java
@@ -0,0 +1,30 @@
+package MyClassWork;
+import java.util.Random;
+public class RandomArrayApp {
+    public static void main(String[] args) {
+        int []array = new int [20];
+        Random random = new Random ();
+
+        for (int i = 0; i <20 ; i++) {
+            array[i] = random.nextInt(11);
+        }
+
+        for (int i = 0; i <20 ; i++) {
+
+            if (array[i] %2  == 0) {
+                if (array[i]%3 == 0) {
+                    System.out.println("fizzbuzz");
+                }
+                System.out.println("fizz");
+            }
+            else if (array[i]%3 == 0){
+                System.out.println("buzz");
+            }
+
+            else {
+                System.out.println();
+            }
+
+        }
+    }
+}
