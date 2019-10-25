1212c13b5a7c71db1e02c6b784db9e4bdd07da98
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-10-25 16:40:19 +0400

    October25 (Task1)

---
+++ src/October25/UniqueNumbers.java
@@ -0,0 +1,16 @@
+package October25;
+
+import java.util.*;
+import java.util.Random;
+
+public class UniqueNumbers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Set<Integer> unique = new HashSet<>();
+
+        while (unique.size()<20) {
+        unique.add(random.nextInt(20+1)-10);
+        }
+        unique.forEach(System.out::println);
+    }
+}
