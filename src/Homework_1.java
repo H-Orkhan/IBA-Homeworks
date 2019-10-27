9e893b427051019cf5f339cf7688a8fbbdcdcb50
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-10-27 22:04:07 +0400

    HomeWork1 Corrected some moments

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -14,24 +14,28 @@ public class Homework_1 {
         for (int i = 0;;i++) {
             System.out.println("Input your number");
             a[i] = scanner.nextInt();
+            if (a[i] <number){
+                System.out.println("Your number is too small. Please, try again.");
+            }
 
-            if (a[i] <number)    System.out.println("Your number is too small. Please, try again.");
-
-            if (a[i] > number)   System.out.println("Your number is too big. Please, try again.");
+            if (a[i] > number){
+                System.out.println("Your number is too big. Please, try again.");
+            }
 
             if (a[i] == number) {
                 System.out.printf("Congratulations,%s\n", name);
                 count = i + 1;
                 break;
-            }}
+            }
+        }
 
         show (a,count);
-        System.out.println("\n*******************Buble sort**************");
-        buble_sort(a,count);
+        System.out.println("\n*******************Bubble sort**************");
+        bubbleSort(a,count);
         show (a,count);
     }
 
-    private static void buble_sort(int[] a,int count) {
+    private static void bubbleSort(int[] a,int count) {
         int temp;
         for (int i = 0; i <count; i++) {
             for (int j = 1; j < (count-i) ; j++) {
@@ -43,5 +47,6 @@ public class Homework_1 {
     private static void show(int[] a,int count) {
         for (int i = 0;i<count;i++) {
             System.out.printf("%d     ",a[i]);
-        }}
+        }
+    }
 }
